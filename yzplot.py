a195177d5215ab62bd180ed0078e8637e9037bef
Shuning Bian <freespace@gmail.com>
2014-08-24 19:11:48 +0100

    Removes imagej_compat from plot_yz_plane_using_scans  because plot_plane_using_scans now handles it

--- yzplot.py
+++ yzplot.py
@@ -11,19 +11,24 @@ def plot_yz_plane_using_scans(scanIDs, yposvec, **imshowkwargs):
 
   In addition to plotting, the result 2D array is also returned.
 
-  If flip_x is given, scanIDs and yposvec are both reversed.
-
   Any unknown kwargs are passed onto imshow.
   """
 
   labels = ('Z position (mm)', 'Y position (mm)')
+
+  if imagej_compat:
+    imshowkwargs['cmap'] = 'gray'
+    imshowkwargs['vmin'] = 0
+    imshowkwargs['vmax'] = 10
+    imshowkwargs['interpolation'] = 'none'
 
   ret = plot_plane_using_scans( scanIDs,
                                 yposvec,
                                 labels=labels,
                                 **imshowkwargs)
 
-  cb = plt.colorbar()
-  cb.set_label('Fluorescence (V)')
+  print 'Implement in-figure colour bars and in-figure axis'
+###  cb = plt.colorbar()
+###  cb.set_label('Fluorescence (V)')
 
   return ret
