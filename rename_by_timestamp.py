3bc2f542abd843ef07460ce8ee6e85a7d12fe290
Shuning Bian <freespace@gmail.com>
2015-12-15 14:08:01 +0000

    Adds checks for existing prefixes, adds dry-run capabilities (rename_by_timestamp.py)

--- rename_by_timestamp.py
+++ rename_by_timestamp.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python
 
+def removeprefix(s):
+  # For files that already have 'XXXX-', where X are digits, we remove
+  # the prefix to avoid producing prefixes like 0001-0002-. This allows us
+  # to run the script in a directory with file that were previously renamed.
+  prefix = s[:5]
+
+  if filter(str.isdigit, prefix) == prefix[:4] and prefix[-1] == '-':
+    return s[5:]
+  else:
+    return s
+
 def timestampcmp(a, b):
   def getts(s):
     # SIOS will name a file with a minute timestamp at the end
@@ -15,27 +26,56 @@ def timestampcmp(a, b):
     for p in parts:
       if len(p) == 4 and p.isdigit():
         return int(p)
-    return None
+    return 99999
 
-  return getts(a) - getts(b)
+  def getscannum(s):
+    # SIOS will also add a scan number which we should use for sorting as
+    # well. This number can be identified as a string in the form of
+    # -XXX- where X are digits, and the number of Xs are variable.
+    parts = s.split('-')
+
+    # b/c it must be surrounded by -, it cannot be the first or last part
+    parts = parts[1:-1]
+
+    for p in parts:
+      if filter(str.isdigit, p) == p:
+        return int(p)
+
+    return 999
+
+  na = getts(a) * 1000 + getscannum(a)
+  nb = getts(b) * 1000 + getscannum(b)
+
+  return na - nb
 
 def main(**kwargs):
   filesvec = kwargs['filestorename']
-  
+  doit = kwargs['doit']
+
   # sort by timestamp
   filesvec.sort(cmp=timestampcmp)
+
+  filesvecnoprefix = map(removeprefix, filesvec)
 
   def f(tt):
     idx,s = tt
     return '%04d-%s'%(idx, s)
 
   # now append nnnn to each file name base on its position in the array
-  newfilesvec = map(f, enumerate(filesvec))
+  newfilesvec = map(f, enumerate(filesvecnoprefix))
 
   from os import rename
   for oldname, newname in zip(filesvec, newfilesvec):
     print oldname, '--->', newname
-    rename(oldname, newname)
+
+    if doit:
+      rename(oldname, newname)
+
+  print ''
+  if doit:
+    print 'Files renamed'
+  else:
+    print 'Files not renamed. Pass `-doit` to commit to above mapping'
 
 def parse_commandline_arguments():
   parser = get_commandline_parser()
@@ -47,6 +87,7 @@ def get_commandline_parser():
   parser = argparse.ArgumentParser(description='Renames file by timestamp')
 
   parser.add_argument('filestorename', nargs='+', help='Files to rename')
+  parser.add_argument('-doit', action='store_true', help='Actually rename files, not just print the mapping')
 
   return parser
 
