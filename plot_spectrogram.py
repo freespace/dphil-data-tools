cb883553f9d262a0ee0273eb6f230622670b1c5b
Shuning Bian <freespace@gmail.com>
2016-03-21 13:37:56 +0000

    Adds binning and colormap commandline option to plot_spectrogram, enforces time monotonicity, ignores DC power

--- plot_spectrogram.py
+++ plot_spectrogram.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 """
-This script takes individual .power.npz files and constructs
-a spectrogram by stacking the power spectrum contained in each file.
+This script takes individual .power.npz files and constructs a spectrogram by
+stacking the power spectrum contained in each file.
+
+When binning is performed, the timestamp of a bin is the timestamp of the fist
+power spectrum in the bin, and power at each frequency is the mean power at
+each frequency of the power spectrums in the bin.
 """
 
 import os.path as op
@@ -19,15 +23,29 @@ def savefig(fig, *args, **kwargs):
   fig.savefig(*args, **kwargs)
 
   print 'Plot saved to file:',args[0]
-  
+
 def plot_spectrogram(**cmdargs):
   powerfilevec = cmdargs['powerfiles']
+  binsize = cmdargs['binsize']
 
+  binpower = None
+  bincount = 0
+  bintrigtime = None
+
+  freqvec0 = None
+
+  lastrigtime = None
   # contains 2-tuple of (trigger time, power spectrum)
   powerspecvec = list()
   for pfile in powerfilevec:
     npzfile = np.load(pfile)
     data = npzfile['data']
+
+    # we delibrately ignore the DC signal, b/c it is very small
+    # and messes up our plotting when logged
+    freqvec = data[1:,0]
+    powervec = data[1:,1]
+
     headerjson = npzfile['header'].item()
 
     import json
@@ -40,20 +58,43 @@ def plot_spectrogram(**cmdargs):
     # ok for now.
     trigtime = datetime.strptime(metadata['trigtime'], '%Y-%m-%d %H:%M:%S.%f')
 
-    powerspecvec.append((trigtime, data))
+    # enforce the condition that power spectrums are monotically into the
+    # future
+    if lastrigtime is not None:
+      assert trigtime > lastrigtime
+    lastrigtime = trigtime
+
+    if binpower is None:
+      binpower = powervec
+      bintrigtime = trigtime
+      bincount = 1
+    else:
+      binpower += powervec
+      bincount += 1
+
+    if freqvec0 is None:
+      freqvec0 = freqvec
+
+    if bincount >= binsize:
+      powerspecvec.append((bintrigtime, binpower/bincount))
+      binpower = None
+      bincount = 0
+
+  # if the last bin is not full it will not have been added, so do it now
+  if binpower is not None:
+    powerspecvec.append((bintrigtime, binpower/bincount))
 
   tstart = powerspecvec[0][0]
   tend = powerspecvec[-1][0]
 
-  # turn these into KHz
-  freqstart = powerspecvec[0][1][0,0]/1000
-  freqend = powerspecvec[0][1][-1,0]/1000
+  # turn these into MHz
+  freqstart = freqvec0[0]/1e6
+  freqend = freqvec0[-1]/1e6
 
-  print 'Got %d power spectrums, starting from %s, ending at %s'%(len(powerspecvec),
-                                                                  tstart,
-                                                                  tend)
+  print 'Binned %d power spectrums into %d bins'%(len(powerfilevec), len(powerspecvec))
+  print '   Time: %s --> %s'%(tstart, tend)
 
-  spectrogram = np.column_stack(map(lambda x:x[1][:,1], powerspecvec))
+  spectrogram = np.column_stack(map(lambda x:x[1], powerspecvec))
 
   tduration = tend - tstart
   tdurationsecs = tduration.total_seconds()
@@ -62,11 +103,26 @@ def plot_spectrogram(**cmdargs):
              aspect='auto',
              interpolation='none',
              extent=[0, tdurationsecs, freqstart, freqend],
-             cmap='hot',
-             norm=mpl.colors.LogNorm())
-  plt.xlabel('Time (s)')
-  plt.ylabel('Frequency (KHz)')
+             cmap=cmdargs['cmap'],
+             norm=mpl.colors.LogNorm(), # logs the colour values
+             origin = 'lower',          # put low freq near (0,0)
+             )
 
+  textvec = ['File: %s'%(powerfilevec[0])]
+  textvec.append('File: %s'%(powerfilevec[-1]))
+  textvec.append('cmap=%s'%(cmdargs['cmap']))
+  if binsize > 1:
+    textvec[-1] += ' binsize=%d'%(binsize)
+
+  plt.text(0.1, 0.1, '\n'.join(textvec))
+
+  plt.xlabel('Time (s)')
+  plt.ylabel('Frequency (MHz)')
+
+  plt.grid()
+
+  # SIOS takes scans every 30 s, so lets have 30 s tics
+  plt.xticks(np.arange(0, tdurationsecs+1, min(30, tdurationsecs/4)))
   plt.title(cmdargs.get('title', ''))
 
   cbar = plt.colorbar()
@@ -98,6 +154,9 @@ def get_commandline_parser():
   parser = argparse.ArgumentParser(description='Plots spectrogram by combining individual power spectrums')
 
   parser.add_argument('-title', default='', help='Plot title')
+  parser.add_argument('-cmap', default='jet', help='Matplotlib colormap to use, defaults to "jet"')
+
+  parser.add_argument('-binsize', type=int, default=1, help='Perform binning with the given bin size. Bin size does not have to be a integer divisor of the number of samples')
 
   parser.add_argument('-pdf', action='store_true', default=False, help='Plot will be saved to PDF instead of being shown')
 
