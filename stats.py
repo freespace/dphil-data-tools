fb413a8893676cad78beedf0ba9e33212833bb1e
Shuning Bian <freespace@gmail.com>
2016-05-28 00:51:23 +0100

    Updates stats.py with smarter FWHM calculation and changes dictionary returned by get_stats

--- stats.py
+++ stats.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 from os.path import basename
 
-import numpy
-from scipy import stats as STATS
+import numpy as np
+from scipy import stats as sstats
 import matplotlib.pyplot as plt
+
+import dphil_paths
 
 from csvtools import CSVReader
 
@@ -12,8 +14,12 @@ def find_width(xvec, yvec, ytarget):
   Finds distance between 2 points on a curve described by xvec and yvec
   where yvec is greater than ytarget.
 
-  The distance is defined as the distance between the left most point above
-  ytarget and the right most point above ytarget.
+  The distance returned is defined to be the distance between the first point of
+  two points to the left of the maximum y value that is below ytarget, and the
+  first of 2 points to the right of the same that is below ytarget. This method
+  avoids some issues with noisy data.
+
+  returns the width, xstart, xend
   """
   def interp(x1,y1,x2,y2):
     rise = y2-y1
@@ -23,27 +29,36 @@ def find_width(xvec, yvec, ytarget):
     dy = ytarget - y1
     return x1+run*dy/rise
 
+  idx_of_max = yvec.argmax()
+
   start = None
   end = None
-  for idx in xrange(len(yvec)-1):
-    if yvec[idx]<=ytarget and yvec[idx+1]>ytarget:
+
+  # get left position
+  idx = idx_of_max
+  while idx > 0:
+    if yvec[idx] <= ytarget and yvec[idx-1] <= ytarget:
       start = interp(
           xvec[idx], yvec[idx],
           xvec[idx+1], yvec[idx+1])
       break
+    idx -= 1
 
-  for idx in xrange(len(yvec)-1,-1, -1): 
-    if yvec[idx]<=ytarget and yvec[idx-1]>ytarget:
+  #get right position
+  idx = idx_of_max
+  while idx < yvec.size-1:
+    if yvec[idx] <= ytarget and yvec[idx+1] <= ytarget:
       end = interp(
           xvec[idx], yvec[idx],
-          xvec[idx-1], yvec[idx-1])
+          xvec[idx+1], yvec[idx+1])
       break
+    idx += 1
 
   if start is not None and end is not None:
     # take the abs value because sometimes our z values
     # are in descending order because a scan is taken
     # backwards
-    return abs(end - start)
+    return abs(end - start), start, end
   else:
     return None
 
@@ -65,7 +80,7 @@ def get_stats(xvec, yvec, noauc=False, asdict=False):
     - area-under-curve (=sum(y-y.mode))
     - FWHM, valid only if you know there is a single peak, and the mode
       correspods to the noise floor.
-    - mode used to calculate FWHM
+    - FWHM_x, contains values that bound the computed FWHM
 
   FWHM is calculated by finding the "half maximum" as 0.5*(peak+mode), the
   width is calculated by finding the left and right edges, where each edge
@@ -73,13 +88,13 @@ def get_stats(xvec, yvec, noauc=False, asdict=False):
   is above the half maximum. some interpolation is done.
   """
 
-  stdev = numpy.std(yvec)
-  mean = numpy.mean(yvec)
-  median = numpy.median(yvec)
-  mode = STATS.mode(yvec)[0][0]
+  stdev = np.std(yvec)
+  mean = np.mean(yvec)
+  median = np.median(yvec)
+  mode = sstats.mode(yvec)[0][0]
   peak = max(yvec)
   miny = min(yvec)
-  auc = sum(yvec - mode)
+  auc = sum(np.abs(yvec))
 
   start = None
   end = None
@@ -89,28 +104,30 @@ def get_stats(xvec, yvec, noauc=False, asdict=False):
   m = median
   # Assume there is only one peak for now
   if peak > m+2*stdev:
-    # use the mode to estimate the noise floor
-    hm = mode + 0.5*(peak-mode)
-    ret.append(find_width(xvec, yvec, hm))
-    ret.append(mode)
+    hm = peak/2.0
+    fwhm, xstart, xend = find_width(xvec, yvec, hm)
+    ret.append(fwhm)
+    ret.append([xstart, xend])
 
   else:
-    ret += [None, mode]
+    ret += [None, None]
 
   if asdict:
     retdict = {}
-    keys = ['max', 'min', 'median', 'mean', 'stdev', 'AUC', 'FWHM', 'mode']
+    keys = ['max', 'min', 'median', 'mean', 'stdev', 'AUC', 'FWHM', 'FWHM_x']
     for key,value in zip(keys, ret):
       retdict[key] = value
     return retdict
   else:
     return ret
 
-def stats(**kwargs):
+def print_stats(**kwargs):
   csvfile = kwargs['csvfile']
-  csvreader = CSVReader(csvfile)
-  mat = csvreader.mat
 
+  from dataloader import DataLoader
+  data = DataLoader(csvfile)
+
+  mat = data.matrix
   xvec = mat[:,0]
 
   def f(key):
@@ -119,47 +136,90 @@ def stats(**kwargs):
   fieldwidth=12
   lblwidth=16
 
-  print csvfile
-  print ' '*lblwidth,
+  statkeys = kwargs['stats']
+  if statkeys is not None:
+    statkeys = map(str.strip, statkeys.split(','))
+
+  csv_out = kwargs['csv_out']
+
+  print csvfile,
+
+  if csv_out:
+    print ',',
+  else:
+    print ' '*lblwidth,
+
   colheaders = ['Max', 'Min', 'Median', 'Mean', 'Stdev', 'AUC', 'FWHM', 'Mode']
-  fmt = '%%-%ds'%(fieldwidth)
-  for hdr in colheaders:
-    print fmt%(hdr) + '|',
-  print ''
-  print ' '*lblwidth,'-'*(len(colheaders)*(fieldwidth+2)-1)
+  if statkeys is not None:
+    colheaders = statkeys
+
+  if not csv_out:
+    fmt = '%%-%ds'%(fieldwidth)
+    for hdr in colheaders:
+      print fmt%(hdr) + '|',
+    print ''
+    print ' '*lblwidth,'-'*(len(colheaders)*(fieldwidth+2)-1)
 
   def p(yvec):
     #fmt = '%%-%dG'%(fieldwidth)
     fmt = '%%-%dG'%(fieldwidth)
-    s = get_stats(xvec, yvec)
+    if statkeys is not None:
+      sdict = get_stats(xvec, yvec, asdict=True)
+      colheaders = statkeys
+      s = map(lambda x: sdict[x], statkeys)
+    else:
+      s = get_stats(xvec, yvec)
     for x in s:
-      if x is None:
+      try:
+        float(x)
+      except:
         x = -1234
-      print fmt%(x)+'|',
+
+      if csv_out:
+        print x,
+        if not s[-1] == x:
+          print ',',
+      else:
+        print fmt%(x)+'|',
 
   ycnt = 1
   fmt = '%%%ds'%(lblwidth)
 
-  colhdrs = csvreader.column_headers
-  for yvec in mat.T:
+  colhdrs = data.xy_labels
+  toplot = mat.T
+  yindex = kwargs['yindex']
+  if yindex is not None:
+    toplot = [toplot[yindex]]
+    colhdrs = [colhdrs[yindex]]
+
+  for yvec in toplot:
     if len(colhdrs) >= ycnt:
       lbl = colhdrs[ycnt-1]
     else:
       lbl = 'Y%d:'%(ycnt)
-    print fmt%(lbl),
+
+    if not csv_out:
+      print fmt%(lbl),
+
     p(yvec)
-    print ''
+
+    if not csv_out:
+      print ''
+
     ycnt+=1
 
 if __name__ == '__main__':
   import argparse
   parser = argparse.ArgumentParser(description='Computes statistics for F410 scan data')
 
+  parser.add_argument('-stats', default=None, type=str, help='stats to compute. Only these stats will be emitted. Multiple stats can be specified, separated by comma')
+  parser.add_argument('-yindex', default=None, type=int, help='Index of Y series data to compute stats for. Defaults to all Y series data.')
+  parser.add_argument('-csv_out', default=False, action='store_true', help='If true output will be in CSV format.')
   parser.add_argument('csvfiles', nargs='+', type=str, help='CSV files to compute statistics for')
 
   args = vars(parser.parse_args())
 
   for csvfile in args['csvfiles']:
     args['csvfile'] = csvfile
-    stats(**args)
+    print_stats(**args)
     print ''
