3cb9cb920d3f6efdba6088afe8b3a8e8b04a38ac
Shuning Bian <freespace@gmail.com>
2016-08-22 16:33:23 +0100

    Adds -vdivide to wzsum.py to allow computation of different vertical sections of an image

--- wzsum.py
+++ wzsum.py
@@ -9,15 +9,19 @@ import dphil_paths
 
 def main(**kwargs):
   datafiles = kwargs['datafiles']
+  vdivide = kwargs['vdivide']
 
   cols = ["Source Filename", "Total PMT Value", "# Pixels", "Pixel PMT Density"]
   print '\t'.join(cols)
 
   for datafile in datafiles:
-    pixsum, npix = get_sum(datafile)
-    print "%s\t%f\t%d\t%f"%(datafile, pixsum, npix, pixsum/npix)
+    statsvec = get_sum(datafile, vdivide=vdivide)
 
-def get_sum(datafile):
+    for idx, stat in enumerate(statsvec):
+      pixsum, npix = stat
+      print "%s[%d]\t%f\t%d\t%f"%(datafile, idx, pixsum, npix, pixsum/npix)
+
+def get_sum(datafile, vdivide=1):
   import numpy as np
   npzfile = np.load(datafile)
   scandata = npzfile['scandata'].item()
@@ -26,7 +30,28 @@ def get_sum(datafile):
     print datafile + ' does not define w axis, not processing.'
     return None
 
-  return np.sum(scandata.matrix), scandata.matrix.size
+  nrows, ncols = scandata.matrix.shape
+
+  # note that this is an integer division, so will under-estimate
+  # the number of rows needed by up to 1
+  rowspersection = nrows // vdivide
+
+  sectionstats = list()
+  for secidx in xrange(vdivide):
+    startrow = secidx * rowspersection
+
+    # because of integer truncation in the calculation of rowspersection,
+    # when we calculate the last section we need to take up any remainders
+    # by going all the way to the bottom
+    if secidx + 1 < vdivide:
+      endrow = startrow + rowspersection
+    else:
+      endrow = nrows
+
+    section = scandata.matrix[startrow:endrow,:]
+    sectionstats.append((np.sum(section), section.size))
+
+  return sectionstats
 
 def parse_commandline_arguments():
   parser = get_commandline_parser()
@@ -36,6 +61,7 @@ def parse_commandline_arguments():
 def get_commandline_parser():
   import argparse
   parser = argparse.ArgumentParser(description='Computes the sum of PMT values over the entire image')
+  parser.add_argument('-vdivide', type=int, default=1, help='Divides the image vertically the specified number of times, outputting a sum for each section. Defaults to 1, equal to summing the whole image as one section')
   parser.add_argument('datafiles', nargs='+', help='WZ data files')
 
   return parser
