a0722a853bb24e8c3090dc8fe5bf01cd687cc08f
Shuning Bian <freespace@gmail.com>
2016-09-10 22:54:39 +0100

    Replaces organise_by_id by organise which is more generic

---
+++ organise.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+"""
+This script organises a file full of SIOS scan files into directories
+based on the phantom ID.
+"""
+
+import os
+
+npz_vec = filter(lambda x:x.endswith('npz'), os.listdir('.'))
+
+
+def get_phantom_id(filename):
+  return filename.split('-')[3]
+
+def get_LUT(filename):
+  return os.path.splitext(filename.split('-')[-1])[0]
+
+def dir_LUT(lut):
+  return lut
+
+def dir_phantom_id(phantom_id):
+  return 'id-'+phantom_id
+
+func_table = dict(by_phantom_id=[get_phantom_id, dir_phantom_id],
+                      by_LUT=[get_LUT, dir_LUT])
+def main(files_to_organise, **kwargs):
+  # key identifies which part of the file we should by, e.g. phantom ID
+  key_set = set()
+
+  sort_by = filter(lambda x:x[1], kwargs.items())[0][0]
+  key_func, dir_func = func_table[sort_by]
+  for fname in files_to_organise:
+    key_set.add(key_func(os.path.basename(fname)))
+
+  print 'Got keys', key_set
+
+  for key in key_set:
+    print 'Organising key '+key
+
+  # create a dir if needed
+    dirname = dir_func(key)
+    if os.path.exists(dirname):
+      assert os.path.isdir(dirname)
+      print '\tdir exists'
+    else:
+      os.mkdir(dirname)
+      print '\tdir created'
+
+    import shutil
+    for fname in files_to_organise:
+      if key_func(os.path.basename(fname)) == key:
+        dst = os.path.join(dirname, fname)
+        shutil.move(fname, dst)
+        print '\tmoved %s to %s'%(fname, dst)
+
+def parse_commandline_arguments():
+  parser = get_commandline_parser()
+  cmdargs = vars(parser.parse_args())
+  return cmdargs
+
+def get_commandline_parser():
+  import argparse
+  parser = argparse.ArgumentParser(description='Organises files produced in my data processing pipeline')
+  parser.add_argument('files_to_organise', type=str, nargs='+', help='Files to organise')
+  g = parser.add_mutually_exclusive_group(required=True)
+  g.add_argument('-by_phantom_id', action='store_true', help='Organises by phantom ID')
+  g.add_argument('-by_LUT', action='store_true', help='Organises by look-up-table')
+
+  return parser
+
+if __name__ == '__main__':
+  parser = get_commandline_parser()
+  cmdargs = parse_commandline_arguments()
+  import sys
+  sys.exit(main(**cmdargs))
