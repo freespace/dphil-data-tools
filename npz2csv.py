74ba499a82d4edf2a41d3f895173a79969bf5735
Shuning Bian <freespace@gmail.com>
2016-10-12 00:59:58 +0100

    Adds a -stdout and -fmt option to npz2csv

--- npz2csv.py
+++ npz2csv.py
@@ -7,16 +7,26 @@ scan data
 
 import numpy as np
 
-def main(npzs_to_convert=None, column_headers=None):
+def main(npzs_to_convert=None, column_headers=None, stdout=None, fmt=None):
   from dataloader import DataLoader
   for npz in npzs_to_convert:
     loader = DataLoader(npz)
 
-    from os.path import splitext, extsep
-    csvname = splitext(npz)[0] + extsep + 'csv'
     header = '\t'.join(column_headers)
-    np.savetxt(csvname, loader.matrix, delimiter='\t', header=header)
-    print 'CSV saved to', csvname
+
+    def tocsv(filename):
+      np.savetxt(filename, loader.matrix, delimiter='\t', header=header, fmt=fmt)
+
+    if stdout:
+      import StringIO
+      out = StringIO.StringIO()
+      tocsv(out)
+      print out.getvalue()
+    else:
+      from os.path import splitext, extsep
+      csvname = splitext(npz)[0] + extsep + 'csv'
+      tocsv(csvname)
+      print 'CSV saved to', csvname
 
 def parse_commandline_arguments():
   parser = get_commandline_parser()
@@ -27,6 +37,8 @@ def get_commandline_parser():
   import argparse
   parser = argparse.ArgumentParser(description='Converts npz data to csv')
   parser.add_argument('-column_headers', type=str, default=[], nargs='+', help='If given will be written as column headers')
+  parser.add_argument('-stdout', action='store_true', default=False, help='If given output will be written to stdout')
+  parser.add_argument('-fmt', type=str, default='%.18e', help='Specifies the output format ala C printf style')
   parser.add_argument('npzs_to_convert', type=str, nargs='+', help='NPZ file to convert')
 
   return parser
